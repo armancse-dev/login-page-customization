67f236d9fcdd3823bd7223818e8c374390e8b446
Arman Hossain <armanhossaincse7@gmail.com>
2023-10-21 17:25:09 +0600

    Plugin Options Page Design

--- login-page-customization.php
+++ login-page-customization.php
@@ -23,6 +23,16 @@
 
   add_action( 'admin_menu', 'lgcw_theme_page_added' );
 
+
+  /*
+  * Style for Plugin Options
+  */
+  function lgcw_admin_style_css(){
+  wp_enqueue_style( 'lgcw_admin_style', plugins_url( 'css/lgcw_admin_style.css', __FILE__ ), false, "1.0.0");
+  }
+
+  add_action('admin_enqueue_scripts', 'lgcw_admin_style_css');
+
   /**
    * Callback plugin function
    */
@@ -30,20 +40,35 @@
   function lgcw_page_create(){
     ?>
       <div class="main_area_lgcw">
-        <div class="body_area_lgcw">
+        <div class="body_area_lgcw lgcw_common">
           <h3 id="title"><?php echo esc_attr( 'Customization Login page' ); ?></h3>
           <form action="options.php" method="post">
             <?php wp_nonce_field( 'update-options' ); ?>
+            <!-- Primary Color -->
             <label for="primary_color_lgcw" name="primary_color_lgcw"><?php echo esc_attr( 'Primary Color' ); ?></label>
-            <input type="color" name="primary_color_lgcw" value="<?php print get_option( 'primary_color_lgcw' ) ?>">
+            <input type="color" name="primary_color_lgcw" value="<?php print get_option( 'primary_color_lgcw' ); ?>">
+            <!-- Main Logo -->
+            <label for="logo_image_lgcw" name="logo_image_lgcw"><?php echo esc_attr( 'Upload Your Logo' ); ?></label>
+            <input type="text" name="logo_image_lgcw" value="<?php print get_option( 'logo_image_lgcwe' ); ?>" placeholder="Paste your Logo URL here">
+
+            <!-- Login page BG Image -->
+            <label for="custom_bg_img_lgcw" name="custom_bg_img_lgcw"><?php print esc_attr( 'Upload your Bacground Image' ); ?></label>
+            <input type="text" name="custom_bg_img_lgcw" value="<?php print get_option('custom_bg_img_lgcw') ?>" placeholder="Paste your Background Image URL here">
+
+            <!-- Login page BG Brightness -->
+            <label for="custom_brightness_bg_lgcw" name="custom_brightness_bg_lgcw"><?php print esc_attr( 'Bacground Brightness {Between 0.1 to 0.9}' ); ?></label>
+            <input type="text" name="custom_brightness_bg_lgcw" value="<?php print get_option('custom_brightness_bg_lgcw') ?>" placeholder="Between 0.1 to 0.9">
 
             <input type="hidden" name="action" value="update" >
-            <input type="hidden" name="page_options" value="primary_color_lgcw" >
+            <input type="hidden" name="page_options" value="primary_color_lgcw, logo_image_lgcw, custom_bg_img_lgcw, custom_brightness_bg_lgcw" >
 
             <input type="submit" name="submit" class="button button-primary" value="<?php _e('Save Changes', 'lgcw'); ?>">
           </form>
         </div>
-        <div class="sidebar_area_lgcw"></div>
+        <div class="sidebar_area_lgcw lgcw_common">
+          <h3 id="title"><?php print esc_attr( 'About Author' ); ?></h3>
+          <p>I'm <strong><a href="https://armandev.com" target="_blank">Arman Hossain</a></strong> a Front End Web developer who is passionate about making error-free websites with 100% client satisfaction. I have a passion for learning and sharing my knowledge with others as publicly as possible. I love to solve real-world problems.</p>
+        </div>
       </div>
 
     <?php
